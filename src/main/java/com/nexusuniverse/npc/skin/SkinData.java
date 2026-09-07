c7884554dd081bf62465640e5f1e6ef9258cb71f
mrlion101az-cell <mrlion101az@gmail.com>
2026-09-07 14:47:37 -0700

    Delete src/main directory

--- src/main/java/com/nexusuniverse/npc/skin/SkinData.java
+++
@@ -1,5 +0,0 @@
-package com.nexusuniverse.npc.skin;
-
-/** A Mojang "textures" property: the base64 texture blob and the signature that goes with it. */
-public record SkinData(String value, String signature, long fetchedAtMillis) {
-}
